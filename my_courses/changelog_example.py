798aaf6c36ca6d467f1e26d6b0f8e4114a9a9452
yanice <yanice.moreau@decathlon.com>
2024-10-22 11:33:31 +0200

    new changelog file

---
+++ my_courses/changelog_example.py
@@ -0,0 +1,18 @@
+"""
+File to show changelog differences based on spacing policies
+"""
+
+
+def my_func_horizontal(param1, param2, param3, param4, param5, param6):
+    pass
+
+
+def my_func_vertical(
+    param1,
+    param2,
+    param3,
+    param4,
+    param5,
+    param6,
+):
+    pass
