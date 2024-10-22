b17e5ee377dd1cb1b0ccc37a69a6ac2d61f2f95f
yanice <yanice.moreau@decathlon.com>
2024-10-22 11:41:40 +0200

    show formatting difference on changelog

--- my_courses/changelog_example.py
+++ my_courses/changelog_example.py
@@ -3,7 +3,7 @@ File to show changelog differences based on spacing policies
 """
 
 
-def my_func_horizontal(param1, param2, param3, param4, param5, param6, param7):
+def my_func_horizontal(param1, param2, param3, new_param1, param4, param5, param6, new_param2, param7):
     pass
 
 
@@ -11,9 +11,11 @@ def my_func_vertical(
     param1,
     param2,
     param3,
+    new_param1,
     param4,
     param5,
     param6,
+    new_param2,
     param7,
 ):
     pass
